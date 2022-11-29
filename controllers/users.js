cb1963a3c9321ccf863f00c437827abae93da201
Jorge Paul <jserranopaul@gmail.com>
2022-11-29 13:59:25 +0100

    PUT and DELETE

--- controllers/users.js
+++ controllers/users.js
@@ -58,6 +58,30 @@ const getSingleUser = async (req, res) => {
 
 const updateUser = async (req, res) => {
   try {
+    const {
+      params: { id },
+      body: { first_name, last_name, age }
+    } = req;
+    /* 
+      Some validations (this is completely outside the scope but useful, in the future something like Express Validator https://express-validator.github.io/docs/
+      or JOI will come in handy)
+        - First name, last name and age are required
+        - First name and last name should be strings
+        - Age should be a number
+    */
+    if (!first_name || !last_name || !age)
+      throw new Error('First name, last name and age are required');
+    if (typeof first_name !== 'string' || typeof last_name !== 'string')
+      throw new Error('First name and last name should be strings');
+    if (typeof age !== 'number') throw new Error('Age should be a number');
+    // Insert - https://www.postgresql.org/docs/current/dml-returning.html
+    const {
+      rows: [updatedUser]
+    } = await db.query(
+      'UPDATE users SET first_name = $1, last_name = $2, age = $3 WHERE id = $4 RETURNING *;',
+      [first_name, last_name, age, id]
+    );
+    res.json(updatedUser);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -65,6 +89,16 @@ const updateUser = async (req, res) => {
 
 const deleteUser = async (req, res) => {
   try {
+    const {
+      params: { id }
+    } = req;
+    const { rowCount } = await db.query('DELETE FROM users WHERE id = $1', [id]);
+    rowCount
+      ? res.json({ success: true, msg: `User with id of ${id} was deleted` })
+      : res.json({
+          success: false,
+          msg: `User with id of ${id} couldn't be deleted or doesn't exist`
+        });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
